342db2f3432a235da7ba03702c8f48b191d3209b
Vcolla <vcollaboni@gmail.com>
2021-08-03 22:54:50 -0300

    Adicionar arquivos de projeto.

---
+++ Csharp.Capitulo01.Sintaxe/VariaveisForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace Csharp.Capitulo01.Sintaxe
+{
+    partial class VariaveisForm
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.operaçõesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aritméticasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resultadoListBox1 = new System.Windows.Forms.ListBox();
+            this.reduzidasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.operaçõesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // operaçõesToolStripMenuItem
+            // 
+            this.operaçõesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aritméticasToolStripMenuItem,
+            this.reduzidasToolStripMenuItem});
+            this.operaçõesToolStripMenuItem.Name = "operaçõesToolStripMenuItem";
+            this.operaçõesToolStripMenuItem.Size = new System.Drawing.Size(94, 24);
+            this.operaçõesToolStripMenuItem.Text = "&Operações";
+            this.operaçõesToolStripMenuItem.Click += new System.EventHandler(this.operaçõesToolStripMenuItem_Click);
+            // 
+            // aritméticasToolStripMenuItem
+            // 
+            this.aritméticasToolStripMenuItem.Name = "aritméticasToolStripMenuItem";
+            this.aritméticasToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.aritméticasToolStripMenuItem.Text = "&Aritméticas";
+            this.aritméticasToolStripMenuItem.Click += new System.EventHandler(this.aritméticasToolStripMenuItem_Click);
+            // 
+            // resultadoListBox1
+            // 
+            this.resultadoListBox1.FormattingEnabled = true;
+            this.resultadoListBox1.ItemHeight = 16;
+            this.resultadoListBox1.Location = new System.Drawing.Point(12, 31);
+            this.resultadoListBox1.Name = "resultadoListBox1";
+            this.resultadoListBox1.Size = new System.Drawing.Size(776, 404);
+            this.resultadoListBox1.TabIndex = 1;
+            // 
+            // reduzidasToolStripMenuItem
+            // 
+            this.reduzidasToolStripMenuItem.Name = "reduzidasToolStripMenuItem";
+            this.reduzidasToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.reduzidasToolStripMenuItem.Text = "&Reduzidas";
+            this.reduzidasToolStripMenuItem.Click += new System.EventHandler(this.reduzidasToolStripMenuItem_Click);
+            // 
+            // VariaveisForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultadoListBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "VariaveisForm";
+            this.Text = "Variáveis e Operações";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem operaçõesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aritméticasToolStripMenuItem;
+        private System.Windows.Forms.ListBox resultadoListBox1;
+        private System.Windows.Forms.ToolStripMenuItem reduzidasToolStripMenuItem;
+    }
+}
+
---
+++ Csharp.Capitulo01.Sintaxe/VariaveisForm.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp.Capitulo01.Sintaxe
+{
+    public partial class VariaveisForm : Form
+    {
+        public VariaveisForm()
+        {
+            InitializeComponent();
+        }
+
+        private void operaçõesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aritméticasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int x = 42;
+            int y;
+            string nome = "Vítor";
+            char letra = 'a';
+            var dataNascimento = new DateTime(2000, 01, 21);
+            bool aprovado = true;
+            var media = 2M;
+            double distancia = 1.2d;
+            float altura = 0.8f;
+            long populacao = 2013;
+            var a = 2;
+            int b = 6, c = 10, d = 13;
+            //c = 9.9m; 
+
+            decimal bim1 = 2.9m;
+
+            string @class = "3A";
+
+            Object objeto = "texto";
+            objeto = 1;
+
+            resultadoListBox1.Items.Clear();
+
+            resultadoListBox1.Items.Add("a =" + a);
+            resultadoListBox1.Items.Add(string.Concat("b =", b));// "c ="
+           // resultadoListBox1.Items.Add(string.Format("c = {0}, d ={1}", c, d));
+            resultadoListBox1.Items.Add(string.Format("c = {0}", c ));
+            resultadoListBox1.Items.Add($"d = {d}");
+            
+            resultadoListBox1.Items.Add($"c * d = {c * d}");            
+            resultadoListBox1.Items.Add($"d / a = {d / a}");            
+            resultadoListBox1.Items.Add($"d % a = {d % a}");            
+            
+            
+
+        }
+
+        private void reduzidasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            resultadoListBox1.Items.Clear();
+
+            var x = 5;
+            resultadoListBox1.Items.Add("x =" + x);
+            //x = x - 3;
+            x -= 3;
+            resultadoListBox1.Items.Add("x =" + x);
+
+
+
+
+        }
+    }
+}
