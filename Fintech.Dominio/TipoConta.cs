e8d3ffe7963e7cd7f25006723bb8ab6ac53a598d
Vcolla <vcollaboni@gmail.com>
2021-08-11 23:01:40 -0300

    Herança

---
+++ Fintech.Dominio/TipoConta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fintech.Dominio
+{
+     public enum TipoConta
+    {
+        ContaCorrente = 1,
+        ContaEspecial = 2,
+        Poupanca =3,
+
+    }
+}
